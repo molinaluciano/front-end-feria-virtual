2be7a10bfff6734f28fb7670fba328433f226a51
Luciano Molina <lucianomolina@MacBook-Pro-de-Luciano-2.local>
2021-11-26 02:43:35 -0300

    view sales and requests for all clients

--- src/page/clientes/cliente_interno/MenuClienteInterno.js
+++ src/page/clientes/cliente_interno/MenuClienteInterno.js
@@ -1,26 +1,27 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import SignOutComponent from "../../../component/SignOutComponent";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SignOutComponent from '../../../component/SignOutComponent';
+import { Link } from 'react-router-dom';
 
 function MenuClienteInterno() {
-  return (
-    <div className="container">
-      <header className="App-header">
-        <h1>Panel de Cliente Interno</h1>
-      </header>
-      <ul className="list-group mb-5">
-        <li className="list-group-item">
-          <a href="#" className="list-group-item list-group-item-action">
-            Mis Compras
-          </a>
-          <a href="#" className="list-group-item list-group-item-action">
-            Crear nueva solicitud
-          </a>
-          <hr />
-          <SignOutComponent />
-        </li>
-      </ul>
-    </div>
-  );
+    return (
+        <div className='container'>
+            <header className='App-header'>
+                <h1>Panel de Cliente Interno</h1>
+            </header>
+            <ul className='list-group mb-5'>
+                <li className='list-group-item'>
+                    <Link
+                        to='/cliente/historial'
+                        className='list-group-item list-group-item-action'
+                    >
+                        Historial
+                    </Link>
+                    <hr />
+                    <SignOutComponent />
+                </li>
+            </ul>
+        </div>
+    );
 }
 
 export default MenuClienteInterno;
